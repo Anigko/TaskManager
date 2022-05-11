816ca333f5bfca6f83d41b73ab6b22ccb01e7a91
Anigko <anigko@yandex.ru>
2022-05-11 21:45:10 +0300

    first commit

--- src/main/java/ru/netology/javacore/TodoServer.java
+++ src/main/java/ru/netology/javacore/TodoServer.java
@@ -1,21 +1,45 @@
 package ru.netology.javacore;
 
-import com.google.gson.Gson;
+import com.jayway.jsonpath.JsonPath;
 
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Scanner;
 
 public class TodoServer {
-    //...
+    final int port = 8089;
 
     public TodoServer(int port, Todos todos) {
-        //...
+        while (true) {
+            try (ServerSocket serverSocket = new ServerSocket(port);
+                 Socket clientSocket = serverSocket.accept();
+                 PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                 BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
+
+                out.printf("New connection accepted. Port: %d%n", clientSocket.getPort());
+
+                final String json = in.readLine();
+
+                String type = JsonPath.read(json, "$.type");
+                String task = JsonPath.read(json, "$.task");
+                if (type.equals("ADD")) {
+                    todos.addTask(task);
+                } else if (type.equals("REMOVE")) {
+                    todos.removeTask(task);
+                }
+                todos.getAllTasks();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+                System.out.println("Connection timeout");
+
+            }
+
+        }
+
     }
 
     public void start() throws IOException {
         System.out.println("Starting server at " + port + "...");
-        //...
     }
 }
--- src/main/java/ru/netology/javacore/Todos.java
+++ src/main/java/ru/netology/javacore/Todos.java
@@ -1,21 +1,31 @@
 package ru.netology.javacore;
 
 import java.util.*;
-import java.util.stream.Collectors;
+
 
 public class Todos {
-    //...
+
+    ArrayList<String> tasks = new ArrayList<>();
 
     public void addTask(String task) {
-        //...
+        tasks.add(task);
+        Collections.sort(tasks);
     }
 
     public void removeTask(String task) {
-        //...
+
+        tasks.remove(task);
     }
 
     public String getAllTasks() {
-        //...
+        StringBuilder sb = new StringBuilder();
+        for (Object o : tasks)
+        {
+            sb.append(o.toString());
+            sb.append("\t");
+        }
+        return sb.toString();
+
     }
 
 }
