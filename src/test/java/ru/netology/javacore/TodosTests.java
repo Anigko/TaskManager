e86ee57a3e803664662d04f84403245499653c72
Anigko <anigko@yandex.ru>
2022-05-22 16:49:33 +0300

    fifth commit

--- src/test/java/ru/netology/javacore/TodosTests.java
+++ src/test/java/ru/netology/javacore/TodosTests.java
@@ -17,25 +17,29 @@ public class TodosTests {
     private final String a = "A";
     private final String b = "B";
     private final String c = "C";
-    private final List<String> list1 = Arrays.asList("A", "B", "C");
-    private final List<String> list2 = Arrays.asList("B", "C");
-    private final String[] vol = {"A", "B", "C", "D", "E", "F", "G"};
+    private final String d = "D";
+    private final String e = "E";
+    private final String f = "F";
+    private final String g = "G";
 
     @Test
     public void taskCheck() {
-        Client task = new Client();
-        String expected = String.valueOf(task.pickRandomChar()); // генерируем задачу
-        String[] taskArray = vol; // создаем список доступных задач
-        int x = Arrays.asList(taskArray).indexOf(expected); // проверяем наличие сгенерированной задачи в массиве и получаем индекс
+        String expected = String.valueOf(Client.pickRandomChar()); // генерируем задачу методом pickRandomChar в классе Client
+        String[] taskArray = {a, b, c, d, e, f, g}; // создаем список доступных задач
+        int x = Arrays.asList(taskArray).indexOf(expected); // проверяем наличие сгенерированной задачи в массиве и возвращаем её индекс
         String result = taskArray[x]; // по индексу получаем задачу из списка доступных задач
         assertEquals(expected, result); // сравниваем
+        if (expected.equals(result)) {
+            System.out.println("Задача сгенерирована верно");
+        }
     }
 
     @Test
     public void testAddMethod() {
+        todos.addTask(c);
         todos.addTask(a);
         todos.addTask(b);
-        todos.addTask(c);
+        List<String> list1 = Arrays.asList(a, b, c);
         assertThat(todos.getTaskList(), is(list1));
     }
 
@@ -45,6 +49,7 @@ public class TodosTests {
         todos.addTask(b);
         todos.addTask(c);
         todos.removeTask(a);
+        List<String> list2 = Arrays.asList(b, c);
         assertThat(todos.getTaskList(), is(list2));
     }
 }
